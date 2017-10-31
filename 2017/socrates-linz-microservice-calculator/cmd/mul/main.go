d31ae08df09f573280318eee394c65f35f967e33
Markus Zimmermann <markus.zimmermann@nethead.at>
2017-10-31 23:25:47 +0100

    Session for Socrates Linz 2017, A Microservice Calculator Using Docker and Kubernetes

---
+++ 2017/socrates-linz-microservice-calculator/cmd/mul/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/gorilla/mux"
+
+	"github.com/symflower/sessions/2017/socrates-linz-microservice-calculator"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	var lock sync.Mutex
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		lock.Lock()
+		defer lock.Unlock()
+
+		var op socra.Mul
+		if err := socra.Decode(r, &op); err != nil {
+			panic(err)
+		}
+
+		time.Sleep(time.Second * 3)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		socra.Encode(w, socra.Result{op.A * op.B})
+	}).Methods("POST")
+
+	socra.RunServer(r)
+}
